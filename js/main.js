8e7f10a98963442754abc71fc4e45f129e24f780
Tony Fischetti <tony.fischetti@gmail.com>
2022-05-03 11:06:33 -0400

    fixed order button bug

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 'use strict';
 
 
+
+const attachEventCallback = (theid, callback, eventType = 'click') => {
+  const tmp = document.getElementById(theid);
+  tmp.addEventListener(eventType, callback);
+};
+
+
+
+
 const start = () => {
+
   let tmp = "working";
   console.log(`JS is ${tmp}`);
-  // console.log(`JS is working`);
+
 };
 
 
